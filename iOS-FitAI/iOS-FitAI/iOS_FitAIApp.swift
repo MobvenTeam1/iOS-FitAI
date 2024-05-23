48bb3ea6380395b23db8b61213328ffab5f755a9
Elif <eliiftum@gmail.com>
2024-05-23 18:47:57 +0300

    [DEV][elif.tum][develop][project files created]

---
+++ iOS-FitAI/iOS-FitAI/iOS_FitAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_FitAIApp.swift
+//  iOS-FitAI
+//
+//  Created by Elif Tum on 23.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_FitAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
