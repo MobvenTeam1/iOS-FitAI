5a0c1f85bde8a11a8c590601b006695b8ad2153e
Elif <eliiftum@gmail.com>
2024-05-23 19:26:56 +0300

    [DEV][elif.tum][develop][project files created & personal infos created]

---
+++ iOS-FitAI/iOS-FitAI/Scene/PersonalInfos/Model/PersonalInfosModel.swift
@@ -0,0 +1,33 @@
+//
+//  PersonalInfosModel.swift
+//  iOS-FitAI
+//
+//  Created by Elif Tum on 23.05.2024.
+//
+
+import Foundation
+
+
+enum PersonalInfosModel {
+    enum PersonalInfosFlow: String {
+        case gender
+        case height
+        case currentWeight
+        case targetWeight
+        case birthDate
+        case targets
+        case plansSelection
+        case healthsIssue
+        case selectedSportsActivites
+        case sportsCount
+        case targetAreas
+        case selectAlergies
+        case dietType
+        case dailyActiveStatus
+        case dailySleepHours
+        case dailyWaterGlass
+    }
+    
+    
+}
+
---
+++ iOS-FitAI/iOS-FitAI/Utils/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  iOS-FitAI
+//
+//  Created by Elif Tum on 23.05.2024.
+//
+
+import Foundation
