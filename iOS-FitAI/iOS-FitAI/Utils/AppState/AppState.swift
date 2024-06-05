1bad7e562452fc440653e40a4a60dce516f11125
atakanahmetyasin <ahmetyasinatakan@gmail.com>
2024-06-05 13:25:39 +0300

    Create AIUpdateFoodSuggestion

--- iOS-FitAI/iOS-FitAI/Utils/AppState/AppState.swift
+++ iOS-FitAI/iOS-FitAI/Utils/AppState/AppState.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 class AppState: ObservableObject {
     @Published var isLoginSuccessful: Bool = false
-    @Published var selectedIndex: Int = 0
+    @Published var selectedIndex: Int = 0 // use it for tabBar
     @Published var isTrainingPlanned: Bool = false
     @Published var isTrainingTapped: Bool = false
     @Published var isNutritionPlanned: Bool = false
     @Published var isNutritionTapped: Bool = false
     @Published  var isOTPCorrect: Bool? = nil
-    @Published var number: String = ""
-    
+    @Published var number: String = "" // use it for gsmnumber
+    //a
+    @Published var isUserUpdateFood = false
+    @Published var selectedFoodIndex: Int? = nil
+    @Published var updateFoodTargetIndex: Int? = nil
+    @Published var updatedFoodItem: FoodItem? = nil
+    //b
 }
