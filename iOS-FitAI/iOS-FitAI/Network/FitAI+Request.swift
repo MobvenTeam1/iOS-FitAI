b8d16d8b0667258aa5e11c15bb7839de9a36e9be
Elif <eliiftum@gmail.com>
2024-06-07 10:02:12 +0300

    [DEV][elif.tum][[login-viewmodel-and-model-created]

--- iOS-FitAI/iOS-FitAI/Network/FitAI+Request.swift
+++ iOS-FitAI/iOS-FitAI/Network/FitAI+Request.swift
@@ -9,19 +9,17 @@ import Foundation
 
 public extension API {
     enum FITAI: Networkable {
-        case login(body: String)
+        case login(params: LoginModel.Request)
         case register(param: RegisterModel.Request)
-        case deneme(deneme: String)
+        
         
         
         public func request() async -> URLRequest {
             switch self {
-            case .login(let body):
-                await getRequest(data: body, path: "User/Login",httpMethod: .post)
+            case .login(let params):
+                await getRequest(data: params, path: "User/Login",httpMethod: .post)
             case .register(let params):
                 await getRequest(data: params, path: "User/Register", httpMethod: .post)
-            case .deneme(let dnm):
-                await getRequest(data: dnm, path: "/eneme", httpMethod: .post)
             }
         }
     }
