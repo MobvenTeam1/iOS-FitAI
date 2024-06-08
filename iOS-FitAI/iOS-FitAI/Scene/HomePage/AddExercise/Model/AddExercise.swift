cecefd695399771944c65e7a7c67d9e379fdc5b5
atakanahmetyasin <ahmetyasinatakan@gmail.com>
2024-06-08 23:56:18 +0300

    Rename assets

--- iOS-FitAI/iOS-FitAI/Scene/HomePage/AddExercise/Model/AddExercise.swift
+++ iOS-FitAI/iOS-FitAI/Scene/HomePage/AddExercise/Model/AddExercise.swift
@@ -17,10 +17,10 @@ struct Exercise: Identifiable {
 
 extension Exercise {
     static let exercises = [
-        Exercise(name: "Yürüyüş", secondaryText: "Düşük Tempo - 1 saatte 65 kcal", imageName: "yürüyüş"),
-        Exercise(name: "Koşu", secondaryText: "1 saatte 178 kcal", imageName: "koşu"),
-        Exercise(name: "Bisiklet", secondaryText: "1 saatte 325 kcal", imageName: "bisiklet"),
-        Exercise(name: "Yüzme", secondaryText: "1 saatte 350 kcal", imageName: "yüzme"),
-        Exercise(name: "Tenis", secondaryText: "1 saatte 280 kcal", imageName: "tenis")
+        Exercise(name: "Yürüyüş", secondaryText: "Düşük Tempo - 1 saatte 65 kcal", imageName: "addwalking"),
+        Exercise(name: "Koşu", secondaryText: "1 saatte 178 kcal", imageName: "addrunning"),
+        Exercise(name: "Bisiklet", secondaryText: "1 saatte 325 kcal", imageName: "addbicycle"),
+        Exercise(name: "Yüzme", secondaryText: "1 saatte 350 kcal", imageName: "addswimming"),
+        Exercise(name: "Tenis", secondaryText: "1 saatte 280 kcal", imageName: "addtennis")
     ]
 }
